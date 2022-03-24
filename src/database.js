4920665960a7d827007dc4d9d29754f4ea727da1
Lucas <lucasazzollinivieira@gmail.com>
2022-03-24 19:27:10 -0300

    fix: ssl encryption added to the connection

--- src/database.js
+++ src/database.js
@@ -6,6 +6,9 @@ const { Pool } = pg;
 
 const connection = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 export default connection;
