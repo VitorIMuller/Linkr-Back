6210676ff78db53d30aa746085a7115fdb60feb3
Vitor <vitormuller66@gmail.com>
2022-03-25 12:06:44 -0300

    fix: bugs fixes

--- src/Routers/usersRouter.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getUser } from "../Controllers/usersController.js";
-const usersRouter = express.Router();
-
-usersRouter.get("/users", getUser);
-
-export default usersRouter;
