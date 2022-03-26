6d6aa21af619400524e1a8cf9e6404b1b82af985
amontheape <mcanedotome@gmail.com>
2022-03-26 19:10:15 -0300

    feat: trending route

---
+++ src/Controllers/trendingController.js
@@ -0,0 +1,14 @@
+import { trending } from "../Repositories/trendingRepository";
+
+export async function getTrending(req, res) {
+  const { limit } = req.params;
+
+  try {
+    const { rows: trendingHashtags } = await trending(limit);
+
+    res.send(trendingHashtags);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+}
--- src/Routers/index.js
+++ src/Routers/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRouter from "./authRouter.js"
 import postsRouter from "./postsRouter.js";
+import trendingRouter from "./trendingRouter.js";
 
 const router = Router();
 
 router.use(authRouter);
 router.use(postsRouter);
+router.use(trendingRouter);
 
 export default router;
